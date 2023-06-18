051426a09e8d127e9ea244bb2ddd51d5f110ed7e
TusharKnwl <tusharofficial23@gmail.com>
2023-06-19 01:27:55 +0530

    added the About section and the SectionWrapper

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,6 +5,30 @@ import { motion } from 'framer-motion'
 import { styles } from '../styles'
 import { services } from '../constants'
 import { fadeIn, textVariant } from '../utils/motion'
+import { SectionWrapper } from '../hoc'
+
+const ServiceCard = ({ title, icon, index }) => {
+  return (
+    <Tilt className= 'xs:w-[250px] w-full'>
+      <motion.div 
+        variants={fadeIn("right","spring", 0.5*index, 0.75)}
+        className='w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card'  
+      >
+        <div
+          options={{
+            max: 45,
+            scale: 1,
+            speed: 450,
+          }}
+          className='bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex flex-col justify-evenly items-center'
+        >
+          <img src={icon} alt={title} className='w-16 h-16 object-contain' />
+          <h3 className='text-white text-[20px] font-bold text-center'>{title}</h3>
+        </div>
+      </motion.div>
+    </Tilt>
+  )
+}
 
 const About = () => {
   return (
@@ -17,18 +41,21 @@ const About = () => {
           Overview
         </h2>
       </motion.div>
-      <motion.p variants={fadeIn("","", 0.1, 0.1)}
-        className='mt-4 text-secondary text-[17px] max-w-3xl leadinf-[30px]'
+
+      <motion.p variants={fadeIn("","", 0.1, 1)}
+        className='mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]'
       >
         Hello, My name is Tushar. I'm a fourth-year Electrical Engineering student at Delhi Technological University with a strong command of C/C++ programming and proficiency in Data Structure algorithms. Over the last year, I have been actively engaged in competitive programming on several platforms such as Leetcode, GFG, CodeChef (3-star), and Hackerrank. In my leisure time, I enjoy watching sci-fi movies, anime, and listening to music.
-You can download my resume on the given below button.
+        You can download my resume on the given below button.
       </motion.p>
 
-      <div className=''> 
-
+      <div className='mt-20 flex flex-wrap gap-10'> 
+        {services.map((service, index) => (
+          <ServiceCard key={service.title} index={index} {...service} />
+        ))}
       </div>
     </>
   )
 }
 
-export default About
+export default SectionWrapper(About, "about")
